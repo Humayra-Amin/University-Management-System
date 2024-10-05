398d553f84d28ba9152ffd95db0fc126865f6ce5
Humayra-Amin <humaema4576@gmail.com>
2024-10-05 12:35:09 +0600

    favicon add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Helmet>
+                <title>UMS | Home</title>
+                <link rel="icon" type="image/jpg" href="/src/assets/images/logos.jpg" />
+            </Helmet>
         </div>
     );
 };
