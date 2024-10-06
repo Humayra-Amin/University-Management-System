e8d1caa6d7a49ca426885337714b9d493cda4b3c
Humayra-Amin <humaema4576@gmail.com>
2024-10-06 21:00:19 +0600

    authprovider added

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,77 +1,77 @@
-// import { createContext, useEffect, useState } from "react";
-// import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, signOut, updateProfile } from "firebase/auth";
-// import auth from "../Firebase/Firebase.config";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import auth from "../Firebase/Firebase.config";
 
 
-// export const AuthContext = createContext(null)
+export const AuthContext = createContext(null)
 
-// const googleProvider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-// const AuthProvider = ({ children }) => {
-//     const [user, setUser] = useState(null)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
 
-//     const [loading, setLoading] = useState(true);
-
-
-//     // create user
-//     const createUser = (email, password) => {
-//         setLoading(true)
-//         return createUserWithEmailAndPassword(auth, email, password)
-//     }
-
-//     //update user profile
-//     const updateUserProfile = (fullname, imageURL) => {
-//         return updateProfile(auth.currentUser, {
-//             displayName: fullname, 
-//             photoURL: imageURL
-//         })
-//     }
+    const [loading, setLoading] = useState(true);
 
 
-//     // sign in user
-//     const signInUser = (email, password) => {
-//         setLoading(true);
-//         return signInWithEmailAndPassword(auth, email, password)
-//     }
+    // create user
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
-//     // google login
-//     const googleLogin = () => {
-//         setLoading(true)
-//         return signInWithPopup(auth, googleProvider)
-//     }
-
-//     // sign out user
-//     const logout = () => {
-//         setUser(null)
-//         signOut(auth)
-//     }
-
-//     useEffect(() => {
-//         const unsubscribe = onAuthStateChanged(auth, (user) => {
-//             if(user) {
-//                 setUser(user)
-//             }
-//             setLoading(false)
-//         })
-//         return () => unsubscribe();
-//     }, [])
+    //update user profile
+    const updateUserProfile = (fullname, imageURL) => {
+        return updateProfile(auth.currentUser, {
+            displayName: fullname, 
+            photoURL: imageURL
+        })
+    }
 
 
-//     const allValues = {
-//         user,
-//         createUser,
-//         signInUser,
-//         googleLogin,
-//         logout,
-//         loading,
-//         updateUserProfile
-//     }
+    // sign in user
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
 
-//     return (
-//         <AuthContext.Provider value={allValues}>
-//             {children}
-//         </AuthContext.Provider>
-//     );
-// };
+    // google login
+    const googleLogin = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
 
-// export default AuthProvider;
+    // sign out user
+    const logout = () => {
+        setUser(null)
+        signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if(user) {
+                setUser(user)
+            }
+            setLoading(false)
+        })
+        return () => unsubscribe();
+    }, [])
+
+
+    const allValues = {
+        user,
+        createUser,
+        signInUser,
+        googleLogin,
+        logout,
+        loading,
+        updateUserProfile
+    }
+
+    return (
+        <AuthContext.Provider value={allValues}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
