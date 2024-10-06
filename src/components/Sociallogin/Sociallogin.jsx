6decc13ae4e6f25c3dceec81c423e489c66b7295
Humayra-Amin <humaema4576@gmail.com>
2024-10-06 21:00:51 +0600

    socialLogin part for login page added

--- src/components/Sociallogin/Sociallogin.jsx
+++ src/components/Sociallogin/Sociallogin.jsx
@@ -1,39 +1,39 @@
-// import { FaGoogle } from "react-icons/fa";
-// import useAuth from "../../hooks/useAuth";
-// import { useLocation, useNavigate } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
 
-// const Sociallogin = () => {
-//     const { googleLogin } = useAuth();
-//     const navigate = useNavigate();
-//     const location = useLocation();
+const Sociallogin = () => {
+    const { googleLogin } = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
 
-//     const from = location?.state || "/student";
+    const from = location?.state || "/student";
 
-//     const handleSocialLogin = (socialProvider) => {
-//         socialProvider()
-//             .then((result) => {
-//                 if (result.user) {
-//                     navigate(from);
-//                 }
-//             });
-//     };
+    const handleSocialLogin = (socialProvider) => {
+        socialProvider()
+            .then((result) => {
+                if (result.user) {
+                    navigate(from);
+                }
+            });
+    };
 
-//     return (
-//         <div className="poppins">
-//             <div className="relative flex justify-center items-center my-6">
-//                 <span className="px-4 text-white text-[16px]">OR Login with</span>
-//             </div>
+    return (
+        <div className="poppins">
+            <div className="relative flex justify-center items-center my-6">
+                <span className="px-4 text-white text-[16px]">OR Login with</span>
+            </div>
 
-//             {/* Social Login */}
-//             <div className="mt-6 text-2xl flex items-center justify-center gap-8">
-//                 <button
-//                     onClick={() => handleSocialLogin(googleLogin)}
-//                     className="flex items-center justify-center w-48 h-12 rounded-md bg-white shadow-lg hover:bg-blue-200">
-//                     <FaGoogle className="text-blue-600" size={30} />
-//                 </button>
-//             </div>
-//         </div>
-//     );
-// };
+            {/* Social Login */}
+            <div className="mt-6 text-2xl flex items-center justify-center gap-8">
+                <button
+                    onClick={() => handleSocialLogin(googleLogin)}
+                    className="flex items-center justify-center w-48 h-12 rounded-md bg-white shadow-lg hover:bg-blue-200">
+                    <FaGoogle className="text-blue-600" size={30} />
+                </button>
+            </div>
+        </div>
+    );
+};
 
-// export default Sociallogin;
+export default Sociallogin;
